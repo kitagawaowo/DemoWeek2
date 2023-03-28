2fec71a2522883bb99cd395545840453776923f6
Usuario de proyectos <proyecto@h51-02.upc.edu.pe>
2023-03-28 16:01:04 -0500

    Bugs with states

--- app/src/main/java/pe/edu/upc/demo/ui/screens/Login.kt
+++ app/src/main/java/pe/edu/upc/demo/ui/screens/Login.kt
@@ -1,28 +1,59 @@
 package pe.edu.upc.demo.ui.screens
 
-import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.ClickableText
 import androidx.compose.material.Button
 import androidx.compose.material.Text
 import androidx.compose.material.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.AnnotatedString
+import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import pe.edu.upc.demo.ui.theme.DemoTheme
 
 @Composable
 fun Login() {
-    Column {
-        Text(text = "Login")
-        TextField(value = "User", onValueChange = {})
-        TextField(value = "Password", onValueChange = {})
+    var username = remember { mutableSetOf(TextFieldValue())}
+    var password = remember { mutableSetOf(TextFieldValue())}
+    Column(
+        modifier = Modifier
+            .padding(20.dp)
+            .fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+
+
+        Text(text = "Login", fontSize = 30.sp)
+        Spacer(modifier = Modifier.height(20.dp))
+
+        TextField(value = username.value, onValueChange = {}, placeholder = { Text("Username") })
+        Spacer(modifier = Modifier.height(20.dp))
+
+        TextField(value = "", onValueChange = {}, placeholder = { Text("Password") })
+        Spacer(modifier = Modifier.height(20.dp))
+
         Button(onClick = { /*TODO*/ }) {
             Text(text = "Sign in")
         }
-        Button(onClick = { /*TODO*/ }) {
-            Text(text = "Sign up")
+        Box(
+            modifier = Modifier.padding(40.dp, 0.dp)
+        ) {
+            Button(
+                onClick = { /*TODO*/ },
+                shape = RoundedCornerShape(50.dp)
+            ) {
+                Text(text = "Sign up")
+            }
         }
-        Button(onClick = { /*TODO*/ }) {
-            Text(text = "Forgot password")
-        }
+        Spacer(modifier = Modifier.height(20.dp))
+        ClickableText(text = AnnotatedString("Forgot password"), onClick = {})
     }
 }
 

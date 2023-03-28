57dc0bed6098363b175d2bc4c66c4be5aeb69c9f
Usuario de proyectos <proyecto@h51-02.upc.edu.pe>
2023-03-28 14:47:42 -0500

    feat(screens): Add Buttons and more text (lazy)

--- app/src/main/java/pe/edu/upc/demo/ui/screens/Login.kt
+++ app/src/main/java/pe/edu/upc/demo/ui/screens/Login.kt
@@ -3,6 +3,7 @@ package pe.edu.upc.demo.ui.screens
 import androidx.compose.foundation.layout.Column
 import androidx.compose.material.Button
 import androidx.compose.material.Text
+import androidx.compose.material.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
 import pe.edu.upc.demo.ui.theme.DemoTheme
@@ -11,8 +12,16 @@ import pe.edu.upc.demo.ui.theme.DemoTheme
 fun Login() {
     Column {
         Text(text = "Login")
+        TextField(value = "User", onValueChange = {})
+        TextField(value = "Password", onValueChange = {})
         Button(onClick = { /*TODO*/ }) {
             Text(text = "Sign in")
+        }
+        Button(onClick = { /*TODO*/ }) {
+            Text(text = "Sign up")
+        }
+        Button(onClick = { /*TODO*/ }) {
+            Text(text = "Forgot password")
         }
     }
 }

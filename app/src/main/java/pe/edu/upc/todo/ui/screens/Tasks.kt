d5d0f3060f9346b4b15e7ffac69ac9d839037091
Usuario de proyectos <proyecto@h51-01.upc.edu.pe>
2023-03-30 13:51:40 -0500

    sesion 2

---
+++ app/src/main/java/pe/edu/upc/todo/ui/screens/Tasks.kt
@@ -0,0 +1,26 @@
+package pe.edu.upc.todo.ui.screens
+
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import pe.edu.upc.todo.ui.theme.ToDoTheme
+
+@Composable
+fun Tasks() {
+    LazyColumn (modifier = Modifier.fillMaxSize()){
+        item {
+            Text(text = "First item")
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    ToDoTheme() {
+        Tasks()
+    }
+}

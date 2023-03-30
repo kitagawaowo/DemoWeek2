c8f4519239ea548a930cde831fda63f7ab1aa1d7
kitagawaowo <uwu20201b980@gmail.com>
2023-03-30 14:32:17 -0500

    states

--- app/src/main/java/pe/edu/upc/todo/ui/screens/Tasks.kt
+++ app/src/main/java/pe/edu/upc/todo/ui/screens/Tasks.kt
@@ -4,37 +4,51 @@ import android.annotation.SuppressLint
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.material.Scaffold
-import androidx.compose.material.Text
-import androidx.compose.material.TopAppBar
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import pe.edu.upc.todo.ui.theme.ToDoTheme
 
 @SuppressLint("UnusedMaterialScaffoldPaddingParameter")
 @Composable
 fun MainScreen() {
+    //var tasks = ArrayList<String>()
+    var tasks = remember {
+        mutableStateOf(ArrayList<String>())
+    }
+
     Scaffold(
         topBar = {
-            TopAppBar() {
+            TopAppBar(
+                elevation = 4.dp,
+                title = {
+                    Text(text = "Todo App")
+                },
+                actions = {
+                    IconButton(onClick = { /*TODO*/ }) {
+                        Icon(Icons.Filled.Add, null)
 
-            }
+                    }
+                }
+            )
         }) {
-            Tasks()
-
+        Tasks(tasks.value)
     }
 }
 
 @Composable
-fun Tasks() {
+fun Tasks(tasks: ArrayList<String>) {
     LazyColumn (modifier = Modifier.fillMaxSize()){
-        item {
-            Text(text = "First item")
+        items(tasks) {task ->
+            Text(text = "$task")
         }
-        items(50) {index ->
-            Text(text = "Item: $index")
-        }
+
     }
 }
 
